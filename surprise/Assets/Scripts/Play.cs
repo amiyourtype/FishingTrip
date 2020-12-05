fc5426bba5fe19ed0ef8b0240ab3d8f74f78ddce
Elexation <72573769+Elexation@users.noreply.github.com>
2020-12-05 14:38:42 -0800

    play button animation

--- surprise/Assets/Scripts/Play.cs
+++ surprise/Assets/Scripts/Play.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Play : MonoBehaviour
 {
+    private int time = 0;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,7 @@ public class Play : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        time++;
+        transform.position = new Vector3(500, 300 + (Mathf.Cos((((time / 200) + 3) * 175) * 4)) * 5, 0);
     }
 }
