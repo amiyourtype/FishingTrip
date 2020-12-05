3c504da913dfcbb329216559ad235a14281e8c69
Brian Mei <m.brian0606@gmail.com>
2020-12-04 17:57:32 -0800

    Added Rarity Instance Variable

--- surprise/Assets/Scripts/Area.cs
+++ surprise/Assets/Scripts/Area.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Area : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
--- surprise/Assets/Scripts/Fish.cs
+++ surprise/Assets/Scripts/Fish.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
+    int rarity;
+    
     // Start is called before the first frame update
     void Start()
     {
