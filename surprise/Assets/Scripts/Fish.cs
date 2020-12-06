bc0ad243a77d4ecbf8848ba5fad0360c91cb9380
Brian Mei <m.brian0606@gmail.com>
2020-12-05 17:15:32 -0800

    Corrected Fish Direction

--- surprise/Assets/Scripts/Fish.cs
+++ surprise/Assets/Scripts/Fish.cs
@@ -18,13 +18,14 @@ public class Fish : MonoBehaviour
     private Vector3 direction;
     private bool hooked;
 
+    private Fisher fisherman;
     private Vector3 mousepos;
     // Start is called before the first frame update
     void Start()
     {
         swim_time = Random.Range(15, 30) + difficulty;
         wait_time = Random.Range(300, 500) - difficulty;
-        direction = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0).normalized / 100 * speed;
+        direction = new Vector3(Random.Range(-10, 11), Random.Range(-10, 11), 0).normalized / 100 * speed;
         hooked = false;
         stamina = 50;
     }
@@ -47,13 +48,14 @@ public class Fish : MonoBehaviour
             {
                 swim_time = Random.Range(0, 30);
                 wait_time = Random.Range(300, 500);
-                direction = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0).normalized / 100 * speed;
+                direction = new Vector3(Random.Range(-10, 11), Random.Range(-10, 11), 0).normalized / 100 * speed;
             }
         }
         else //Code to run for minigame
         {
             stamina += difficulty / 10;
-            transform.position += new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0).normalized / 50 * speed;
+            transform.position += direction * 2; //change position of fish
+            transform.position = (transform.position - fisherman.transform.position) * (float) 0.95 + fisherman.transform.position; //make position drift towards hook
             mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             float dist = Vector3.Distance(transform.position, new Vector3(mousepos.x, mousepos.y, 0));
             if (Input.GetMouseButtonUp(0) && dist < ringRadius)
@@ -62,13 +64,19 @@ public class Fish : MonoBehaviour
             }
             if (stamina <= 0)
             {
-                Debug.Log("i've been caught >:(");
+                Debug.Log("i've been caught >:C");
                 Destroy(this.gameObject); //Destroy(this.line) later
             }
             else if (stamina > 100)
             {
                 Debug.Log("i'm free B)");
                 Destroy(this.gameObject);
+            }
+            swim_time -= 1;
+            if (swim_time <= 0)
+            {
+                direction = new Vector3(Random.Range(-10, 11), Random.Range(-10, 11), 0).normalized / 100 * speed;
+                swim_time = Random.Range(10, 20);
             }
         }
     }
@@ -77,6 +85,7 @@ public class Fish : MonoBehaviour
     {
         if(!collision.gameObject.GetComponent<Fisher>().hooked)
         {
+            fisherman = collision.gameObject.GetComponent<Fisher>();
             Debug.Log("ive been hooked :(");
             hooked = true;
         }
