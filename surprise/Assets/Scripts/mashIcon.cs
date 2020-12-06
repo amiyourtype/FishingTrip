e85da615c3667ca92cd8dd803408863a5b762786
amiyourtype <35315317+amiyourtype@users.noreply.github.com>
2020-12-06 02:16:10 -0800

    mashing icon follows fish

---
+++ surprise/Assets/Scripts/mashIcon.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mashIcon : MonoBehaviour
+{
+    public Fisher fisher;
+    public SpriteRenderer spriteRenderer;
+    public Sprite mash1;
+    public Sprite mash2;
+    private bool animator;
+    public int timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 5;
+        animator = false;
+        //spriteRenderer.sprite = mash2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(fisher.hooked)
+        {
+            transform.position = new Vector3(fisher.target.transform.position.x, fisher.target.transform.position.y - 40, 10);
+            timer -= 1;
+            if(timer <= 0)
+            {
+                if(animator)
+                {
+                    spriteRenderer.sprite = mash2;
+                    animator = false;
+                }
+                else 
+                {
+                    spriteRenderer.sprite = mash1;
+                    animator = true;
+                }
+                timer = 5;
+            }
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 110);
+        }
+    }
+}
