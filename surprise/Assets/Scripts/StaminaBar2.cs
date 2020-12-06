e72791399067af447673de8d366726de3ff5a5b1
amiyourtype <35315317+amiyourtype@users.noreply.github.com>
2020-12-06 01:13:32 -0800

    stamina bar works
    skippidy boop bop, skippidy ay, my oh my what a wonderful day. plenty of sunshine, in my way, wonderful feeling, wonderful day!

---
+++ surprise/Assets/Scripts/StaminaBar2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaminaBar2 : MonoBehaviour
+{
+    public double size;
+    public Fisher fisher;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.localScale = new Vector3(1,size,1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(fisher.hooked)
+        {
+        size = fisher.target.stamina/100f * 1.2f;
+        transform.localScale = new Vector3(1,(float)size,1);
+        //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+    }
+}
